2e71a09cef83506729c9effbe29bf8e959ed7823
Gabriela Fijalkowska <gabrielafijalkowska@gmail.com>
2017-09-09 08:22:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  
+}
+
+function destructivelyPrependKitten(name) {
+  
+}
+
+function destructivelyRemoveLastKitten() {
+  
+}
